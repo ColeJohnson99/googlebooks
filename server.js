81524891956536cb0de50d102810b73466041b1f
Your Name <you@example.com>
2020-05-07 19:07:27 -0600

    Further setup, including adding mongodb and mongoose installs, partial mongodb setup in server.js

--- server.js
+++ server.js
@@ -3,6 +3,25 @@ const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+const express = require("express");
+const mongojs = require("mongojs");
+
+// Initialize Express
+const app = express();
+
+// Database configuration
+// Save the URL of our database as well as the name of our collection
+const databaseUrl = "google";
+const collections = ["books"];
+
+// Use mongojs to hook the database to the db variable
+const db = mongojs(databaseUrl, collections);
+
+// This makes sure that any errors are logged if mongodb runs into an issue
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
