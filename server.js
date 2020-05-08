d801d8d9cda9df5f4245a28adae012fc4c4b9c6d
Your Name <you@example.com>
2020-05-07 19:12:58 -0600

    More mongodb and server.js setup, created instructions markdown

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-const express = require("express");
+const mongoose = require("mongoose");
 const mongojs = require("mongojs");
 
 // Initialize Express
