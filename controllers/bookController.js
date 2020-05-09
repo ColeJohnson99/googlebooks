4098d6198715165c718666e66b32d33d70c9279c
Your Name <you@example.com>
2020-05-09 11:54:56 -0600

    Setup db interaction, findAll, create, update, delete functions added into bookController.js file

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -4,6 +4,34 @@ module.exports = {
     findAll: function(req, res) {
         db.books
             .find(req.query)
+            .sort({ date: -1 })
+            .then(dbmodel => res.json(dbmodel))
+            .catch(err => res.status(422).json(err));
+    },
+    findById: function(req, res) {
+        db.books
+            .findById(req.params.id)
+            .then(dbmodel => res.json(dbmodel))
+            .catch(err => res.status(422).json(err));
+    },
+    create: function(req, res) {
+        db.Book
+          .create(req.body)
+          .then(dbModel => res.json(dbModel))
+          .catch(err => res.status(422).json(err));
+    },
+    update: function(req, res) {
+        db.books
+            .findOneAndUpdate({_id: req.params.id }, req.body)
+            .then(dbmodel => res.json(dbmodel))
+            .catch(err => res.status(422).json(err));
+    },
+    remove: function(req, res) {
+        db.books
+            .findById({_id: req.params.id })
+            .then(dbmodel => dbmodel.remove())
+            .then(dbModel => res.json(dbmodel))
+            .catch(err => res.status(422).json(err));
     }
     
-}
+};
