a910c477879b5a84cb8dc8ec44cd9fce7e70c747
Your Name <you@example.com>
2020-05-12 09:23:58 -0600

    Setup page footer with name and some styling

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+    <footer className = "page-footer">
+        <div className = "container-fluid text-center">
+            <p>Cole Johnson</p>
+        </div>
+    </footer>
+);
+
+export default Footer;
